30db1e26cda5105e88c93977616b7f57d42c1957
Simply_ <icedeveloper247@gmail.com>
2026-05-04 18:24:47 +0100

    feat: create Room mongoose model with name, capacity, type, and movie reference fields

---
+++ server/Models/room.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Room name is required!"],
+        unique: true,
+        trim: true
+    },
+    capacity: {
+        type: Number,
+        required: [true, "Room capacity is required!"],
+        trim: true
+    },
+    type: {
+        type: String,
+        required: [true, "Room type is required!"],
+        trim: true,
+        enum: ["standard", "vip", "premium"]
+    },
+    movies: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: "Movie",
+    }
+});
+
+
+const Room = mongoose.model('Room', roomSchema);
+
+
+module.exports = Room;
